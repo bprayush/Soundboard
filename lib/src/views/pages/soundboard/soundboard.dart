0bcfcc2cc66f5b9d70887f554f0700b97f707675
bprayush <bprayush@yahoo.com>
2019-10-06 10:52:48 +0545

    Tried testing admob, but didn't work have to look into admob again later.

---
+++ lib/src/views/pages/soundboard/soundboard.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Soundboard extends StatefulWidget {
+  @override
+  _SoundboardState createState() => _SoundboardState();
+}
+
+class _SoundboardState extends State<Soundboard> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(),
+    );
+  }
+}
